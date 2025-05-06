05d5e364ca9319d502b4f5b461d956c5c8d7301e
cntrvsy <benintangana@gmail.com>
2025-05-06 07:28:20 +0300

    example on joins

--- examples/c02-join.rs
+++ examples/c02-join.rs
@@ -1,4 +1,4 @@
-use pretty_sqlite::print_table;
+use pretty_sqlite::{print_rows, print_table};
 use rusqlite::Connection;
 use rust_xp_sqlite::db_utils::create_schema;
 
@@ -8,6 +8,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // --Create Schema
     create_schema(&conn)?;
+
+    // -- Insert Org
+    let mut stmt = conn.prepare(
+        "INSERT INTO org (name) 
+            VALUES (?1) RETURNING id",
+    )?;
+
+    let org_id = stmt.query_row(["Acme, Inc"], |row| row.get::<_, i64>(0))?;
+
+    // -- Insert Person
+    let names = &["Jen", "Bob", "Sally", "Mike"];
+    for (idx, name) in names.iter().enumerate() {
+        let org_id = if idx % 2 == 0 { Some(org_id) } else { None };
+        conn.execute(
+            "INSERT INTO person (name, org_id, yob)
+                VALUES (?1, ?2, ?3)",
+            (name, &org_id, 2000),
+        )?;
+    }
+
+    // -- Select Join
+    let select_sql = "SELECT person.id, person.name, person.yob, org.name as org_name
+        FROM person
+        INNER JOIN org ON person.org_id = org.id
+        WHERE org.id = :org_id";
+    let mut stmt = conn.prepare(select_sql)?;
+    let mut rows = stmt.query(&[(":org_id", &org_id)])?;
+
+    print_rows(rows)?;
 
     Ok(())
 }
